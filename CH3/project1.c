be89b45962d640a7f7e0337e4b908a1d58980b4b
Max-JigangZhu <jigang.zhu@uwaterloo.ca>
2022-09-24 18:09:59 -0400

    Added project1.c source code under CH3
    description

--- CH3/project1.c
+++ CH3/project1.c
@@ -2,8 +2,10 @@
 
 int main(void) {
     int mm, dd, yyyy;
+
     printf("Enter a date (mm/dd/yyyy): ");
     scanf("%d/%d/%d", &mm, &dd, &yyyy);
+    
     printf("You entered the date %d%.2d%.2d\n", yyyy, mm, dd);
 
     return 0;
