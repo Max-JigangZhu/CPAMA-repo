f275aa817defc479a6dcc2828006e77b8d7c2705
Max-JigangZhu <jigang.zhu@uwaterloo.ca>
2022-09-24 18:07:36 -0400

    Added exercise5.c comment and source code under CH3
    description

--- CH3/exercise5.c
+++ CH3/exercise5.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /*
-12.300000 45 0.600000
+12.300000 45 0.600000 
 */
 
 int main(void) {
-    float x, y;
     int i;
+    float x, y;
     scanf("%f%d%f", &x, &i, &y);
     printf("%f %d %f\n", x, i, y);
 
