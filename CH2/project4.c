a02b935abc602ec89550a5fe81b2d7198faa9999
Max-JigangZhu <jigang.zhu@uwaterloo.ca>
2022-09-24 20:36:20 -0400

    Added project4.c source code under CH2
    description

---
+++ CH2/project4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define tax 0.05
+
+int main(void) {
+    float amount;
+    printf("Enter an amount: %0.2f\n", amount);
+    scanf("%f", &amount);
+    amount += amount*tax;
+    printf("With tax added: $%0.2f\n", amount);
+}
