6ee792935a2423ec8d63393696e255ffe668e1b2
Max-JigangZhu <jigang.zhu@uwaterloo.ca>
2022-09-24 20:35:58 -0400

    Added project3.c comment and source code under CH2
    description

---
+++ CH2/project3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define pi 3.14
+
+/*
+#include and #define are directives
+printf, scanf, v = ..., and return are statements
+*/
+
+int main(void) {
+    float r, v;
+
+    printf("Enter the radius of the sphere (in centimeters): ");
+    scanf("%f", &r);
+
+    v = 4.0f/3.0f*pi*r*r*r;
+    
+    printf("Enter a radius: %0.2f\n", r);
+    printf("The volume of the sphere is %0.2fcm\n", v);
+
+    return 0;
+}
