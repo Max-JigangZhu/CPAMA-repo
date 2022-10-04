c7e96d3d7eb7610b86569ec3dcf46cceeec370b4
Max-JigangZhu <jigang.zhu@uwaterloo.ca>
2022-10-04 14:58:44 -0400

    Added a comment
    description

---
+++ CH02/exercise01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("hello, world\n");
+    return 0; 
+}
+/*without excuting return 0 will cause a warning: 
+    "control reaches end of non-void fuction" with -Wall flag in c89 standard*/
