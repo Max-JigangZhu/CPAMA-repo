7101fa668739541083073764096ffadf1b8d8006
Max-JigangZhu <jigang.zhu@uwaterloo.ca>
2022-10-04 18:14:46 -0400

    Renamed CH2 and CH3 and all the exercisex.c and projectx.c under them to CH02 and CH03 and exercise0x.c and project0x.c; used c89 standard to compile all the .c source code file under CH02 and CH03 and updated the .c file based on the compiling result
    description

--- CH02/project04.c
+++ CH02/project04.c
@@ -3,7 +3,7 @@
 
 int main(void) {
     float amount;
-    printf("Enter an amount: %0.2f\n", amount);
+    printf("Enter an amount: ");
     scanf("%f", &amount);
     amount += amount*tax;
     printf("With tax added: $%0.2f\n", amount);
