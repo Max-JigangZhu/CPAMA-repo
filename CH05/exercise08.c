c24dc884b9e3951ae7bb7f8e7a4081335c9ec6c3
Max-JigangZhu <jigang.zhu@uwaterloo.ca>
2022-10-04 15:00:22 -0400

    Deleted boolean type value teenager and updated the compound statement
    description

--- CH05/exercise08.c
+++ CH05/exercise08.c
@@ -2,13 +2,11 @@
 
 int main(void) {
     int age;
-    _Bool teenager;
 
     printf("Enter an age: ");
     scanf("%d", &age);
 
-    teenager = age >= 13 && age <= 19;
-    printf("%d\n", teenager);
+    age >= 13 && age <= 19 ? printf("true\n") : printf("false\n");
 
     return 0;
 }
