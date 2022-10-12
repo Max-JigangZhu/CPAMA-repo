3aecb4b304831d97bcecf10c5d15e389fa95583f
Max-JigangZhu <jigang.zhu@uwaterloo.ca>
2022-10-12 15:36:48 -0400

    Added exercise10.c under CH06
    description

---
+++ CH06/exercise10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+   int i = 0;   
+    for(; i < 10; i++) {
+        if (i == 5) {
+            goto HERE; /* continue; */
+        }
+        printf("%d ", i);
+HERE: ;
+    }
+
+    return 0;
+}
