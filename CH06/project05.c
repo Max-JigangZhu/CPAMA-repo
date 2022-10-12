474e4fb0e1ca814b1b272581952d0c56666990e4
Max-JigangZhu <jigang.zhu@uwaterloo.ca>
2022-10-12 18:14:04 -0400

    Added project05.c source code under CH06
    description

---
+++ CH06/project05.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int i;
+
+    printf("Enter an integer: ");
+    scanf("%d", &i);
+    printf("The reversal is: ");
+
+    do {
+        printf("%d", i % 10);
+        i /= 10;
+    } while (i > 0);
+    printf("\n");
+
+    return 0;
+}
