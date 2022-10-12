1e91addd8af3f3352b10c56550088dd2be59132a
Max-JigangZhu <jigang.zhu@uwaterloo.ca>
2022-10-12 14:11:40 -0400

    Modified exercise02.c under CH04 to be more formatted
    description

--- CH04/exercise02.c
+++ CH04/exercise02.c
@@ -1,5 +1,5 @@
 /* 
 In C89 if either of the i and j are negative, the division can either be rounded up or rounded down.
     Therefore, the result of the two expression may differ.
-In C99, they always have the same value since the division always towards 0
+In C99, they always have the same value since the division always towards 0.
 */
